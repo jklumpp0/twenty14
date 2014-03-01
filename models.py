abcb58696fbb5ea8f0721a9065f19d7dfc41dcaa
Jared <jklumpp0@jdesk.(none)>
2014-03-01 10:06:19 -0800

    Added a model manager and unit tests

--- models.py
+++ models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class AnswerManager(models.Manager):
+    def for_user(self, user):
+        records = Answer.objects.filter(user=user)
+        return records
+
 class Answer(models.Model):
-        result = models.BooleanField()
-        date = models.DateField(auto_now_add = True)
-        user = models.ForeignKey(User)
-    
-        def __unicode__(self):
-            return "YES" if self.result else "NO"
+    result = models.BooleanField()
+    date = models.DateField(auto_now_add = True)
+    user = models.ForeignKey(User)
+    objects = AnswerManager()
+
+    def __unicode__(self):
+        return "YES" if self.result else "NO"
+
 

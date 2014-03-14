2ae581cb6f1000b257fd743384bed525db2f9dff
Jared <jklumpp0@jdesk.(none)>
2014-03-13 22:58:13 -0700

    adjust widths

--- static/twenty14/js/graph.js
+++ static/twenty14/js/graph.js
@@ -10,11 +10,15 @@ function setupGraph() {
             return d.value;
         };
 
-        var xScale = d3.scale.linear().domain([0, d3.max(data, xAccessor) * 11]).range([0, width]);
+        var xMax = d3.max(data, xAccessor);
+        var padding = 0.1;
+        var barWidth = 10;
+        var xTotalPadding = xMax * padding;
+        var xScale = d3.scale.linear().domain([0, xMax * barWidth + (xTotalPadding)]).range([0, width]);
         var yScale = d3.scale.linear().domain([d3.max(data, yAccessor), 0]).range([height, 0]);
 
         var barTransform = function(d) {
-            return "translate(" + (xScale(d.day) * 10) + ", " + (height - yScale(d.value)) + ")";
+            return "translate(" + (xScale(d.day * barWidth + padding)) + ", " + (height - yScale(d.value)) + ")";
         };
 
         var chart = d3.select("#history");
@@ -25,7 +29,7 @@ function setupGraph() {
                         .enter()
                         .append("g")
                         .attr("transform", barTransform);
-        bar.append("rect").attr("width", xScale(10)).attr("height", function(d) { return yScale(yAccessor(d)); });
+        bar.append("rect").attr("width", function(d) { return xScale(barWidth); }).attr("height", function(d) { return yScale(yAccessor(d)); });
     };
     console.log("HI");
     new Graph();

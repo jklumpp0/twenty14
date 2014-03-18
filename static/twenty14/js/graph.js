72deb7c69d53f5596b1ca2bd4f452b28fddbe755
Jared Klumpp <jklumpp@gatech.edu>
2014-03-18 13:35:20 -0700

    Added a transition for the bar graph and re-oriented it right side up. Added some styling.

--- static/twenty14/js/graph.js
+++ static/twenty14/js/graph.js
@@ -1,24 +1,25 @@
 function setupGraph() {
     var Graph = function() {
-        var width = 800;
+        var width = parseInt(d3.select('body').style('width'), 10);
         var height = 400;
-        var xAccessor = function(d) {
-            return d.day;
-        };
 
         var yAccessor = function(d) {
             return d.value;
         };
 
-        var xMax = d3.max(data, xAccessor);
-        var padding = 0.1;
+        var xMax = data.length;
+        var padding = 1;
         var barWidth = 10;
         var xTotalPadding = xMax * padding;
         var xScale = d3.scale.linear().domain([0, xMax * barWidth + (xTotalPadding)]).range([0, width]);
         var yScale = d3.scale.linear().domain([d3.max(data, yAccessor), 0]).range([height, 0]);
 
         var barTransform = function(d) {
-            return "translate(" + (xScale(d.day * barWidth + padding)) + ", " + (height - yScale(d.value)) + ")";
+            return "translate(" + (xScale(d.day * barWidth + padding * d.day)) + ", " + (height - yScale(d.value)) + ")";
+        };
+
+        var barStart = function(d) {
+            return "translate(" + (xScale(d.day * barWidth + padding * d.day)) + ", " + height + ")";
         };
 
         var chart = d3.select("#history");
@@ -28,8 +29,12 @@ function setupGraph() {
                         .data(data)
                         .enter()
                         .append("g")
+                        .attr("transform", barStart);
+        bar.transition()
+            .duration(3000)
                         .attr("transform", barTransform);
-        bar.append("rect").attr("width", function(d) { return xScale(barWidth); }).attr("height", function(d) { return yScale(yAccessor(d)); });
+        var rect = bar.append("rect").attr("width", xScale(barWidth))
+                        .attr("height", function(d) { return yScale(yAccessor(d)); });
     };
     console.log("HI");
     new Graph();
